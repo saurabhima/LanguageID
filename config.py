4e809f9a4f033865bf6d0954f3f936b98a78de26
Saurabh Singh <saurabhima@gmail.com>
2017-10-24 19:12:44 -0400

    Added FFT Plot Creation and Data Augmentation Code

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 #This file contains the Global COnfiguration which will be used in the project including location of datasets, Model Hyperparameters etc
+DATASET_CSV_DIR='/home/mt0/16CS60D03/LanguageID/code'
+DATASET_CSV_FILE='trainingData.csv'
+PLOTS_DIR='/home/mt0/16CS60D03/LanguageID/dataset/Training/plots'
+PLOTS_LIST_DIR='/home/mt0/16CS60D03/LanguageID/dataset/Training'
+PLOTS_LIST_FILE='plot_list.txt'
+DATASET_MP3_DIR='/home/mt0/16CS60D03/LanguageID/dataset/Training'
